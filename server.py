6e898d384927e51686a1a96c1859e0589924611a
NaomiatLibrary <ray.oshikawa@gmail.com>
2021-03-29 20:57:04 +0900

    衝突判定なしのゲーム

--- server.py
+++ server.py
@@ -1,9 +1,61 @@
 from aiohttp import web
 import socketio
+import math
+import threading
 
-sio = socketio.AsyncServer()
+sio = socketio.AsyncServer(async_mode='aiohttp',logger=True, engineio_logger=True)
 app = web.Application()
 sio.attach(app)
+
+WIDTH=600
+HEIGHT=1000
+BALL_RADIUS=50
+
+class Stone:
+    def __init__(self,camp,v,theta):
+        self.camp=camp
+        self.y=0
+        self.x=WIDTH/2
+        if self.camp=="AI":
+            self.y=HEIGHT
+        self.v=v
+        self.theta=theta #thetaの方向に速度がv
+        self.radius=BALL_RADIUS
+    def move(self):
+        if self.v>0:
+            self.v=max(0,self.v-0.05)
+        self.x+=self.v*math.cos(math.radians(self.theta))
+        self.y+=self.v*math.sin(math.radians(self.theta))
+        if self.x>WIDTH: #x軸方向に反転
+            self.x=2*WIDTH-self.x
+            self.theta=2*90-self.theta
+        if self.x<0: #x軸方向に反転
+            self.x=-self.x
+            self.theta=2*90-self.theta
+        if self.y>HEIGHT: #y軸方向に反転
+            self.y=2*HEIGHT-self.y
+            self.theta=-self.theta
+        if self.y<0: #y軸方向に反転
+            self.y=-self.y
+            self.theta=-self.theta
+    def encode(self):
+        return {'x': self.x,
+            'y': self.y,
+            'radius':self.radius,
+            'camp':self.camp}
+
+
+
+
+situations={}#盤面ごとに存在するカーリングの球の状態を記録する
+
+async def background_task():
+    """Example of how to send server generated events to clients."""
+    count = 0
+    while True:
+        await sio.sleep(10)
+        count += 1
+        await sio.emit('my_response', {'data': 'Server generated event'})
 
 async def index(request):
     """Serve the client-side application."""
@@ -13,13 +65,54 @@ async def index(request):
 @sio.event
 def connect(sid, environ):
     print("connect ", sid)
+    situations[sid]=[]
 
 @sio.event
-async def game_start(sid, data):
+async def game_start(sid, data): 
     print("message ", data['test'])
+    #ここで準備
+    #球を打っていいよの合図
+    await sio.emit('your_turn',room=sid)
+
+
+    
+@sio.event
+async def hit_stone(sid,data):
+    print("hit_stone")
+    situations[sid].append( Stone("you",data["velocity"],data["theta"]) )
+    while True:
+        await sio.emit('move_stones', {'stones': [stone.encode() for stone in situations[sid]]},room=sid)
+        sio.sleep(10)
+        print("move!")
+        stillmove = False
+        for stone in situations[sid]:
+            stone.move()
+            if stone.v!=0:
+                stillmove=True
+        if not stillmove:
+            break
+    #相手が打つ
+    situations[sid].append( Stone("AI",5,-40) )
+    while True:
+        await sio.emit('move_stones', {'stones': [stone.encode() for stone in situations[sid]]},room=sid)
+        sio.sleep(10)
+        print("move!")
+        stillmove = False
+        for stone in situations[sid]:
+            stone.move()
+            if stone.v!=0:
+                stillmove=True
+        if not stillmove:
+            break
+    await sio.emit('your_turn',room=sid)
+        
+        
+
 
 @sio.event
 def disconnect(sid):
+    print("situation:",situations.pop(sid,"Not Found"))
+    print("situations:",situations)
     print('disconnect ', sid)
 
 app.router.add_static('/static', 'static')
@@ -27,4 +120,5 @@ app.router.add_static('/node_modules', 'node_modules')
 app.router.add_get('/', index)
 
 if __name__ == '__main__':
+    #sio.start_background_task(background_task)
     web.run_app(app)
--- static/game.js
+++ static/game.js
@@ -1,15 +1,76 @@
 'use strict';
-
-const socket = io();
+const socket = io.connect();
 const canvas = $('#canvas-2d')[0];
 const context = canvas.getContext('2d');
 const playerImage = $('#player-image')[0];
-let movement = {};
-
-function gameStart(){
-    console.log("gameStart!");
-    var data={"test":"yes"};
-    socket.emit("game_start",data);
+let playercursor=90;
+let data_storage=null;
+//球を打つ
+function clearAndWriteStage(){
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.lineWidth = 10;
+    context.beginPath();
+    context.rect(0, 0, canvas.width, canvas.height);
+    context.closePath();
+    context.stroke(); 
 }
 
-socket.on('connect', gameStart);
+function rewriteSituation(data){
+    clearAndWriteStage();
+    if(data===null)return;
+    Object.values(data.stones).forEach((stone)=>{
+        console.log(stone);
+        context.beginPath();
+        if(stone.camp==='you')context.fillStyle = 'red';
+        else context.fillStyle = 'blue';
+        context.arc(stone.x,canvas.height-stone.y,stone.radius,0,2*Math.PI);
+        context.fill();
+    });
+}
+function writePointer(theta){
+    console.log(theta);
+    rewriteSituation(data_storage);//再描写（いい方法があれば変えたい…）
+    var length=300;
+    context.lineWidth = 5;
+    var endx=canvas.width/2+length*Math.cos(theta*(Math.PI/180));
+    var endy=canvas.height-length*Math.sin(theta*(Math.PI/180));
+    context.beginPath();
+    context.moveTo(canvas.width/2,canvas.height);
+    context.lineTo(endx,endy);
+    context.closePath();
+    context.stroke();
+    context.lineWidth = 10;
+}
+$(document).ready(function(){
+    console.log("Page is loaded");
+    clearAndWriteStage();
+    socket.on('connect', (data)=>{
+        console.log("gameStart!",data);
+        var data={"test":"yes"};
+        socket.emit("game_start",data);
+    });
+    socket.on('your_turn', (data) =>{
+        playercursor=90;
+        writePointer(playercursor);
+        $(document).on('keydown keyup', (event) => {
+            //console.log("keydown,keyup");
+            if(event.key === 'ArrowLeft'&& event.type === 'keydown'){
+                if(playercursor<170)playercursor+=1;
+                writePointer(playercursor);
+            }
+            if(event.key === 'ArrowRight'&& event.type === 'keydown'){
+                if(playercursor>10)playercursor-=1;
+                writePointer(playercursor);
+            }
+            if(event.key === ' ' && event.type === 'keydown'){
+                console.log("hit stone");
+                socket.emit("hit_stone",{"theta":playercursor,"velocity":7});
+                $(document).off('keydown keyup');
+            }
+        });
+    });
+    socket.on('move_stones', (data) =>{
+        rewriteSituation(data);
+        data_storage=data;
+    });
+});
