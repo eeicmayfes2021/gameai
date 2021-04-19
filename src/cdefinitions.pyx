ae96399bafdd16dd2a8cdcc69fe13b83e85348bc
usa <61061134+usagrada@users.noreply.github.com>
2021-04-19 15:16:43 +0900

    衝突を運動量保存で計算

--- src/cdefinitions.pyx
+++ src/cdefinitions.pyx
@@ -45,10 +45,32 @@ cdef class Stone:
             return
         #衝突している時
         #運動方程式解いた
-        e= [(self.x[0]-other.x[0])/dist,(self.x[1]-other.x[1])/dist]
-        t=(self.v[0]*e[0]+self.v[1]*e[1])-(other.v[0]*e[0]+other.v[1]*e[1])
-        self.v=[self.v[0]-t*e[0],self.v[1]-t*e[1]]
-        other.v=[other.v[0]+t*e[0],other.v[1]+t*e[1]]
+        e= [(other.x[0]-self.x[0])/dist,(other.x[1]-self.x[1])/dist]
+        # t=(self.v[0]*e[0]+self.v[1]*e[1])-(other.v[0]*e[0]+other.v[1]*e[1])
+        # self.v=[self.v[0]-t*e[0],self.v[1]-t*e[1]]
+        # other.v=[other.v[0]+t*e[0],other.v[1]+t*e[1]]
+        
+        # 座標を回転させた後の値
+        # aがself, bがotherに対応
+        vax =  e[0] * self.v[0] + e[1] * self.v[1]
+        vay = -e[1] * self.v[0] + e[0] * self.v[1]
+        vbx = e[0] * other.v[0] + e[1] * other.v[1]
+        vby = -e[1] * other.v[0] + e[0] * other.v[1]
+
+        self.v[0]  = e[0] * vbx - e[1] * vay
+        self.v[1]  = e[1] * vbx + e[0] * vay
+        other.v[0] = e[0] * vax - e[1] * vby
+        other.v[1] = e[1] * vax + e[0] * vby
+        
+
+        while dist>self.radius+other.radius or dist==0:
+            self.move()
+            other.move()
+            dist=sqrt( (self.x[0]-other.x[0])*(self.x[0]-other.x[0])+(self.x[1]-other.x[1])*(self.x[1]-other.x[1]))
+            if self.v[0] == 0 and self.v[1]== 0 and dist<self.radius+other.radius:
+                print("cannnot move but the distance is too near!!")
+                break
+        
     cpdef return_dist(self):
         center=[WIDTH/2,HEIGHT/2]
         dist=sqrt( (self.x[0]-center[0])*(self.x[0]-center[0])+(self.x[1]-center[1])*(self.x[1]-center[1]))
@@ -90,6 +112,7 @@ cpdef movestones(list stones):#ボトルネック#400ループぐらいする
                 isend=False
                 for j in range(len(stones)):
                     if i!=j and (j,i) not in iscollisionlist:
+                        #iscollisionlist.append([j, i])
                         stones[i].collision(stones[j])
         if isend:
             return
