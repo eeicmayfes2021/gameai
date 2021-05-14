c0e72e835d41b23b83f35a9b454b1901fa174fa6
NaomiatLibrary <ray.oshikawa@gmail.com>
2021-05-15 01:06:27 +0900

    awsupload sync

--- awsupload/awsupload.sh
+++ awsupload/awsupload.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 while true; do
     basepass=$(pwd)
-    modelpass="${basepass}/models/*"
-    for foldername in $modelpass; do
-        f=${foldername##*/}
-        echo $f
-        if grep -q $f ${basepass}/awsupload/uploadedfolders.txt; then
-            echo uploaded;
-        else
-            echo upload;
-            aws s3 cp ${foldername} s3://eeic2021-gameai-1/${f} --recursive
-            echo $f >> ${basepass}/awsupload/uploadedfolders.txt
-        fi    
-    done
+    modelpass="${basepass}/models"
+    aws s3 sync ${modelpass} s3://eeic2021-gameai-1
+    #for foldername in $modelpass; do
+    #    f=${foldername##*/}
+    #    echo $f
+    #    if grep -q $f ${basepass}/awsupload/uploadedfolders.txt; then
+    #        echo uploaded;
+    #    else
+    #        echo upload;
+    #        aws s3 cp ${foldername} s3://eeic2021-gameai-1/${f} --recursive
+    #        echo $f >> ${basepass}/awsupload/uploadedfolders.txt
+    #    fi    
+    #done
     sleep 180;
 done
